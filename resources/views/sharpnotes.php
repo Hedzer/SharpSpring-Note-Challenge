f3ff8c5f21113b35a9772d6a325d09fd808b35b4
Hedzer <hedzer87@gmail.com>
2016-04-09 23:59:39 -0400

    SharpNotes App View, Experimentation
    ? Experimenting with r.js building, making sure it all will eventually
    work.  Still need to see if there's a dev vs production mode in Lumen

--- resources/views/sharpnotes.php
+++ resources/views/sharpnotes.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- Start Meta Tags-->
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<!-- End Meta Tags-->
 		<title>Sharp Notes</title>
+		<link rel="stylesheet" type="text/css" href="/css/common/em-based-reset.css">
+		<link rel="stylesheet" type="text/css" href="/css/sharpnotes/structure.css">
+		<!-- Can easily be variable for theme swapping -->
+		<link rel="stylesheet" type="text/css" href="/css/sharpnotes/theme.css">
+		<link href="https://fonts.googleapis.com/css?family=Merienda+One" rel="stylesheet" type="text/css">
 	</head>
 	<body>
-		<script data-main="/js/apps/sharpnotes.requires.js" src="/js/libs/require.min.js"></script>
+		<!-- <script data-main="/js/apps/sharpnotes.requires.js" src="/js/libs/require.min.js"></script> -->
+		<script data-main="/js/apps/sharpnotes.app.built.js" src="/js/libs/require.min.js"></script>
 	</body>
 </html>
