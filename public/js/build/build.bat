9c2e7039cc1cb7070dc356cdea9eaeebf7f306a2
Hedzer <hedzer87@gmail.com>
2016-04-10 00:00:57 -0400

    r.js Optimization Tooling
    ? Decided to optimize after all, will now have to mess with the tooling.

---
+++ public/js/build/build.bat
@@ -0,0 +1,3 @@
+node r -o config.js
+copy %cd%\libs\require.min.js+%cd%\apps\sharpnotes.app.built.js %cd%\apps\sharpnotes.app.built2.js
+pause
---
+++ public/js/build/config.js
@@ -0,0 +1,10 @@
+({
+	baseUrl:'../',
+	paths: {
+		'ES5-Shim': 'shims/es5-shim.min',
+		'Viewport-Shim': 'shims/vminpoly.min',
+		'SharpNotes': 'apps/sharpnotes.app'
+	},
+	name: 'apps/sharpnotes.requires',
+	out: '../apps/sharpnotes.app.built.js'
+})
