28e3067904af7ed8be4f2a5afd2386b1327cdfaa
Hedzer <hedzer87@gmail.com>
2016-04-10 00:37:54 -0400

    r.js Optimization Tooling Changes
    + Added require into the build correctly

--- public/js/build/build.bat
+++ public/js/build/build.bat
@@ -1,3 +1,2 @@
 node r -o config.js
-copy %cd%\libs\require.min.js+%cd%\apps\sharpnotes.app.built.js %cd%\apps\sharpnotes.app.built2.js
 pause
--- public/js/build/config.js
+++ public/js/build/config.js
@@ -1,10 +1,12 @@
 ({
 	baseUrl:'../',
 	paths: {
+		'requireLib': 'libs/require.min',
 		'ES5-Shim': 'shims/es5-shim.min',
 		'Viewport-Shim': 'shims/vminpoly.min',
 		'SharpNotes': 'apps/sharpnotes.app'
 	},
+	include: 'requireLib',
 	name: 'apps/sharpnotes.requires',
 	out: '../apps/sharpnotes.app.built.js'
 })
