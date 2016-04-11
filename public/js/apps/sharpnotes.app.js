2b7a3ebebb3c1305e21f5d0d3b2e867d21da105d
Hedzer <hedzer87@gmail.com>
2016-04-10 23:28:30 -0400

    SharpNotes App, Componentized
    + Turned into a bona-fide component, if desired, can be instanced more
    than once. Why? if it were integrated into a larger application could be
    loaded on demand by calling new SharpNotes()

--- public/js/apps/sharpnotes.app.js
+++ public/js/apps/sharpnotes.app.js
@@ -1,23 +1,49 @@
 define(
 	[
 		'frameworks/syrup',
-		'components/navbar'
+		'components/app',
+		'components/navbar',
+		'components/page',
+		'components/matchstick'
 	],
-	function(Syrup, navbar){
-		var App = {
-			Navigation:(function(){
-				var navigation = new navbar();
-				navigation.addTo(document.body);
-				navigation.brand = '#Sharp Notes';
-				return navigation;			
-			})(),
-			Page:(function(){
+	function(Syrup, app, navigation, page, matchstick){
+		function SharpNotes(){
+			app.call(this);
+			this.class = 'SharpNotes';
+			this.className = 'SharpNotes';
+			//Navigation
+			this.add(new navigation()).as('Navigation').with(function(){
+				this.brand = '#Sharp Notes';
+				//Logout Button
+				this.add(new matchstick()).as('Logout').with(function(){
+					this.classList.add('logout-button');
+					this.classList.add('cursive');
+					this.Body.Text.textContent = 'Logout';
+					this.Head.style.background = 'black';
+					this.Body.style.background = 'red';
+				});
+			});
+			this.add(new page()).as("Page").with(function(){
 
-			})(),
-			Controller:(function(){
-
-			})()
+			});
 		}
+		SharpNotes.prototype = Object.create(app.prototype);
+		SharpNotes.prototype.constructor = SharpNotes;
+		return SharpNotes;
 
+		// var App = {
+		// 	Navigation:(function(){
+		// 		var navigation = new navbar();
+		// 		navigation.addTo(document.body);
+		// 		navigation.brand = '#Sharp Notes';
+		// 		return navigation;			
+		// 	})(),
+		// 	Page:(function(){
+
+		// 	})(),
+		// 	Controller:(function(){
+
+		// 	})()
+		// }
 	}
 );
