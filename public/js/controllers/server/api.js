d2d7e3da97e31b52d5f5262cffcfbbf92a1a8cce
Hedzer <hedzer87@gmail.com>
2016-04-12 23:35:49 -0400

    Server API Controller, Added

--- public/js/controllers/server/api.js
+++ public/js/controllers/server/api.js
@@ -1,6 +1,23 @@
 define(
 	function(){
 		var API = {};
+		var token = null;
+		Object.defineProperty(API, 'token', {
+			get:function(){
+				if (window.sessionStorage){
+					if (!token){
+						token = window.sessionStorage.getItem('token');
+					}
+					return token;
+				}
+			},
+			set:function(value){
+				token = value;
+				if (window.sessionStorage){
+					window.sessionStorage.setItem('token', value);
+				}
+			}
+		});
 		return API;
 	}
 );
