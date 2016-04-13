a58447505085d8e091d4afc1bb98560946de9266
Hedzer <hedzer87@gmail.com>
2016-04-12 23:34:44 -0400

    SharpNotes, Slight Refactor
    + Modularized some of the views, made them their own component.

--- public/js/apps/sharpnotes.app.js
+++ public/js/apps/sharpnotes.app.js
@@ -2,31 +2,17 @@ define(
 	[
 		'frameworks/syrup',
 		'components/app',
-		'components/navbar',
+		'components/sharpnotes/navbar',
 		'components/page',
-		'components/matchstick',
-		'components/responsiveImage',
 		'components/sharpnotes/noteList'
 	],
-	function(Syrup, app, navigation, page, matchstick, responsiveImage, list){
+	function(Syrup, app, navigation, page, list){
 		function SharpNotes(){
 			app.call(this);
 			this.class = 'SharpNotes';
-			this.className = 'SharpNotes';
+			this.classList.add('SharpNotes');
 			//Navigation
-			this.add(new navigation()).as('Navigation').with(function(){
-				this.brand = '#Sharp Notes';
-				//Logout Button
-				this.add(new matchstick()).as('Logout').with(function(){
-					var logoutButton = this;
-					var buttonText = logoutButton.Body.Text;
-					//classes to add
-					['logout-button', 'cursive'].forEach(function(c){
-						logoutButton.classList.add(c);
-					});
-					this.Body.Text.textContent = 'Logout';
-				});
-			});
+			this.add(new navigation()).as('Navigation');
 			//Page
 			this.add(new page()).as('Page').with(function(){
 				this.add(new list()).as('NoteList').with(function(){
@@ -37,20 +23,5 @@ define(
 		SharpNotes.prototype = Object.create(app.prototype);
 		SharpNotes.prototype.constructor = SharpNotes;
 		return SharpNotes;
-
-		// var App = {
-		// 	Navigation:(function(){
-		// 		var navigation = new navbar();
-		// 		navigation.addTo(document.body);
-		// 		navigation.brand = '#Sharp Notes';
-		// 		return navigation;			
-		// 	})(),
-		// 	Page:(function(){
-
-		// 	})(),
-		// 	Controller:(function(){
-
-		// 	})()
-		// }
 	}
 );
--- public/js/components/sharpnotes/navbar.js
+++ public/js/components/sharpnotes/navbar.js
@@ -1,7 +1,29 @@
 define(
 	[
 		'components/navbar',
-		'controllers/sharpnotes/navbar'
+		'controllers/sharpnotes/navbar',
+		'components/matchstick',
+
 	],
-	function(){}
+	function(navbar, controller, matchstick){
+		function sharpnotesNavbar(){
+			navbar.call(this);
+			this.class = 'sharpnotesNavbar';
+			this.classList.add('sharpnotes-navbar');
+			this.brand = '#Sharp Notes';
+			//Logout Button
+			this.add(new matchstick()).as('Logout').with(function(){
+				var logoutButton = this;
+				var buttonText = logoutButton.Body.Text;
+				//classes to add
+				['logout-button', 'button-a','transitions-background-color', 'cursive'].forEach(function(c){
+					logoutButton.classList.add(c);
+				});
+				this.Body.Text.textContent = 'Logout';
+			});
+		}
+		sharpnotesNavbar.prototype = Object.create(navbar.prototype);
+		sharpnotesNavbar.prototype.constructor = sharpnotesNavbar;
+		return sharpnotesNavbar;
+	}
 );
