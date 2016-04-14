f7503ae640ca94798cdb13dc8912b882f1815ab5
Hedzer <hedzer87@gmail.com>
2016-04-14 17:07:28 -0400

    Login Controller, Added Response Handling

--- public/js/controllers/server/login.js
+++ public/js/controllers/server/login.js
@@ -4,7 +4,22 @@ define(
 		'controllers/server/api'
 	],
 	function(ajax, api){
-		function ServerLoginController(credentials, onSuccess, onFailure){
+		function ServerLoginController(credentials, onSuccess, onFailure, onInvalid){
+			var success = function(response, xhr){
+				if (typeof onSuccess === 'function'){
+					onSuccess(response, xhr);
+				}
+			};
+			var failure = function(response, xhr){
+				if (typeof onFailure === 'function'){
+					onFailure(response, xhr);
+				}
+			};
+			var invalid = function(response, xhr){
+				if (typeof onInvalid === 'function'){
+					onInvalid(response, xhr);
+				}
+			};
 			ajax.post(
 				'/auth/login',
 				{
@@ -13,13 +28,32 @@ define(
 				}
 			)
 			.success(function(response, xhr){
-				if (typeof onSuccess === 'function'){
-					onSuccess(response, xhr);
+				try {
+					var parsed = JSON.parse(response);
+					if (parsed.auth && parsed.token){
+						api.token = parsed.token;
+						success(parsed);
+						return;
+					}
+					if (parsed.email || parsed.password){
+						invalid(parsed);
+						return
+					}
+					failure('Authentication Failed');
+				}catch(e){
+					failure('Unknown Server Error');
 				}
 			})
-			.failure(function(response, xhr){
-				if (typeof onFailure === 'function'){
-					onFailure(response, xhr);
+			.failure(function(response){
+				try {
+					var parsed = JSON.parse(response);
+					if (parsed.email || parsed.password){
+						invalid(parsed);
+					} else {
+						failure('Unknown Server Error');
+					}
+				}catch(e){
+					failure(response);
 				}
 			});
 		}
