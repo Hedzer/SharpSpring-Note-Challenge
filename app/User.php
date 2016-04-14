a60d4c6437c0ba3b455e44e4b68474a2d67a1410
Hedzer <hedzer87@gmail.com>
2016-04-13 23:29:05 -0400

    User Model, Allowed Passwords Field
    + It's not the best idea, but I was having trouble accessing it even
    through appropriate channels.

--- app/User.php
+++ app/User.php
@@ -1,19 +1,19 @@
 <?php
-
 namespace App;
-
 use Illuminate\Auth\Authenticatable;
 use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-
+/**
+ * Class User
+ * @package App
+ */
 class User extends Model implements
     AuthenticatableContract,
     AuthorizableContract
 {
     use Authenticatable, Authorizable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -22,13 +22,12 @@ class User extends Model implements
     protected $fillable = [
         'name', 'email',
     ];
-
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
     protected $hidden = [
-        'password',
+        // 'password',
     ];
 }
